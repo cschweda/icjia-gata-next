15698230d33c0d205d7dc519352ffaa49f49b2ff
cschweda <cschweda@gmail.com>
2019-04-05 12:02:42 -0500

    fix: add back IsAfter and endOfDay

--- pages/funding/index.vue
+++ pages/funding/index.vue
@@ -54,8 +54,8 @@
 <script>
 import jsonata from 'jsonata'
 import format from 'date-fns/format'
-// import isAfter from 'date-fns/is_after'
-// import endOfDay from 'date-fns/end_of_day'
+import isAfter from 'date-fns/is_after'
+import endOfDay from 'date-fns/end_of_day'
 import { mapGetters } from 'vuex'
 
 import { EventBus } from '@/event-bus.js'
